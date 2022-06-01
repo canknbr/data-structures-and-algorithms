fb477aceb542c9980dd4414919cb1bb0d1330806
Can Kanbur <cankanbur1@gmail.com>
2022-06-01 14:48:20 +0300

    .

---
+++ data-structure-linked-list/main.js
@@ -0,0 +1,53 @@
+let firstLinkedList = {
+  head: {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null,
+        },
+      },
+    },
+  },
+};
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    let newNodefor = new Node(value);
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: this.head,
+    };
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+const linkedList = new LinkedList(firstLinkedList);
