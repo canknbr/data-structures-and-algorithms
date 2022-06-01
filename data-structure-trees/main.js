4498e7c29925f7eb9844729179a5dec06fa4467c
Can Kanbur <cankanbur1@gmail.com>
2022-06-01 16:06:01 +0300

    .

---
+++ data-structure-trees/main.js
@@ -0,0 +1,109 @@
+// function BinaryTree(value) {
+//   this.value = value;
+//   this.left = null;
+//   this.right = null;
+// }
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let currentNode = this.root;
+    while (true) {
+      if (value === currentNode.value) return undefined;
+      if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = newNode;
+          return this;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+  lookup(value) {
+    if (this.root === null) return false;
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
+  remove(value) {
+    if (this.root === null) return false;
+    let currentNode = this.root;
+    let parentNode = null;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        // We have a match, get to work!
+        // Option 1: No right child:
+        if (currentNode.right === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left;
+          } else {
+            // if parent > current value, make current left child a child of parent
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left;
+            }
+          }
+        }
+        // Option 2: Right child which doesnt have a left child
+        else if (currentNode.right.left === null) {
+          currentNode.right.left = currentNode.left;
+          if (parentNode === null) {
+            this.root = currentNode.right;
+          } else {
+            // if parent > current, make right child of the left the parent
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.right;
+            }
+          }
+        } else {
+          // find the inorder predecessor of the node to be deleted
+          let parentOfSuccessor = currentNode;
+          let successor = currentNode.right;
+          while (successor.left !== null) {
+            parentOfSuccessor = successor;
+            successor = successor.left;
+          }
+          // parentOfSuccessor is now the parent of the inorder predecessor
+          // successor is the in
+        }
+      }
+    }
+  }
+}
